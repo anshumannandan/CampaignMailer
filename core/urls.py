3fdc499f6651478d35721ce4e83f8d74d83d34db
anshumannandan <anshuman21xxx034@akgec.ac.in>
2023-05-16 18:01:42 +0530

    endpoint to unsubscribe users

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
+from .views import UnsubscribeView
 
 urlpatterns = [
-
+    path('unsub/<int:pk>/', UnsubscribeView.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -1,1 +1,12 @@
-from rest_framework import views
+from rest_framework import generics, response
+from . models import User
+
+
+class UnsubscribeView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_active = False
+        instance.save()
+        return response.Response({'message' : f'{instance.email} Unsubscribed'})
